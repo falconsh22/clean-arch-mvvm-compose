529a2dc1f4eee4b99f52f24e4b3a1eae1b1687b1
falconsh22 <shahin@itranslate.com>
2022-05-24 16:30:06 +0200

    Interface fetching artists, only first page for start

--- app/src/main/java/com/shahin/cleancompose/data/remote/searchArtists/SearchArtistsArtistsRepositoryImpl.kt
+++ app/src/main/java/com/shahin/cleancompose/data/remote/searchArtists/SearchArtistsArtistsRepositoryImpl.kt
@@ -1,11 +1,23 @@
 package com.shahin.cleancompose.data.remote.searchArtists
 
+import com.shahin.cleancompose.data.remote.artist.models.Artist
+import com.shahin.cleancompose.data.remote.searchArtists.models.response.SearchResponse
 import com.shahin.cleancompose.data.remote.searchArtists.services.SearchArtistsApiService
 import com.shahin.cleancompose.domain.repositories.searchArtists.SearchArtistsRepository
+import com.shahin.cleancompose.network.NetworkResponse
+import com.shahin.cleancompose.network.NetworkWrapper
 import javax.inject.Inject
 
 class SearchArtistsArtistsRepositoryImpl @Inject constructor(
     private val searchArtistsApiService: SearchArtistsApiService
-): SearchArtistsRepository {
+): NetworkWrapper(), SearchArtistsRepository {
+
+    override suspend fun searchArtistByName(artistName: String): NetworkResponse<SearchResponse<Artist>> {
+        return networkResponseOf {
+            searchArtistsApiService.searchArtistByNameQuery(
+                artistName = artistName
+            )
+        }
+    }
 
 }
--- app/src/main/java/com/shahin/cleancompose/domain/repositories/searchArtists/SearchArtistsRepository.kt
+++ app/src/main/java/com/shahin/cleancompose/domain/repositories/searchArtists/SearchArtistsRepository.kt
@@ -1,4 +1,11 @@
 package com.shahin.cleancompose.domain.repositories.searchArtists
 
+import com.shahin.cleancompose.data.remote.artist.models.Artist
+import com.shahin.cleancompose.data.remote.searchArtists.models.response.SearchResponse
+import com.shahin.cleancompose.network.NetworkResponse
+
 interface SearchArtistsRepository {
+
+    suspend fun searchArtistByName(artistName: String): NetworkResponse<SearchResponse<Artist>>
+
 }
